4d0371dd5d698573e08bf76fc9168fb4480644e1
vijay_wm@yahoo.com <vijay_wm@yahoo.com>
2019-08-30 15:02:06 +0530

    adds navbar partial and ag-grid

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const bodyParser = require("body-parser");
 const nunjucks = require("nunjucks");
 const app = express();
 
+const columnDefs = [
+  { headerName: "Make", field: "make" },
+  { headerName: "Model", field: "model" },
+  { headerName: "Price", field: "price" }
+];
+
+const data = [
+  ["2017", 10, 11, 12, 13],
+  ["2018", 20, 11, 14, 13],
+  ["2019", 30, 15, 12, 13]
+];
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -14,6 +26,14 @@ nunjucks.configure("views", {
 
 app.get("/", function(req, res) {
   res.render("index.html");
+});
+
+app.get("/one", function(req, res) {
+  res.json({ columns: columns, data: data });
+});
+
+app.use(function(req, res) {
+  res.json({ columns: columnDefs, data: data });
 });
 
 app.listen(3000, function() {
